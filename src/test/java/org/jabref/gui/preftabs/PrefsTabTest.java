3f2eb57dff59508f9252459efb9817465dc9e8c2
Pedromauad <phdmauad@gmail.com>
2018-06-19 10:37:10 -0300

    Versão Funcionando no SonarQube

--- src/test/java/org/jabref/gui/preftabs/PrefsTabTest.java
+++ src/test/java/org/jabref/gui/preftabs/PrefsTabTest.java
@@ -1,1 +1,21 @@
 package org.jabref.gui.preftabs;
+/*
+import java.awt.BorderLayout;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import javax.swing.BorderFactory;
+import javax.swing.ButtonGroup;
+import javax.swing.JCheckBox;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JTextField;
+import org.jabref.logic.l10n.Localization;
+import org.jabref.preferences.JabRefPreferences;
+import com.jgoodies.forms.builder.DefaultFormBuilder;
+import com.jgoodies.forms.layout.FormLayout;
+import org.junit.experimental.categories.Category;
+
+@Category(GUItest.class)
+class GroupsPrefsTab extends JPanel implements PrefsTab {
+*/
